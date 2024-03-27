a485f4bd411f7c149c7b8c8273722573d96f4266
thecodeprofessor <nathan@abourbih.com>
2024-03-27 13:40:35 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NathansBookStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
